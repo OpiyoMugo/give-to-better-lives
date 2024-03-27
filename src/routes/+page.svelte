5035008b71039792b9f4e9417a99b98fa86ab0a5
wes-ednilson <wesleydulo99@gmail.com>
2024-03-27 10:20:04 +0300

    switch from static to dymanic rendering

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import HomePage from '../routes/home/+page.svelte';
-</script>
-
-<HomePage />
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	
-	preprocess: vitePreprocess(),
 	kit: {
-		
-		
-		adapter: adapter({ 
-			fallback: 'app.html'
-		}) 
-		
+		adapter: adapter() 
 	}
 };
 
