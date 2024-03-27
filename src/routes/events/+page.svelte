8e6a3ec6c0c542a8dfd1e70611187cded3103ad1
wes-ednilson <wesleydulo99@gmail.com>
2024-03-27 14:58:51 +0300

    add image

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,21 +1,21 @@
 <script>
 	let showMore = false;
 	const images = [
-		'https://futbol4life.com/wp-content/uploads/2021/03/7-ngo-help-children-intro-750x330.jpg',
+		'children.jpg',
 		'https://www.habitatforhumanity.org.uk/wp-content/uploads/2017/04/Poverty-and-education-in-east-Africa-.jpg',
 		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZgVH0rAelJ2l7Ty-gU06ngwpGDnmeRJuUkw&usqp=CAU',
 		'https://1.bp.blogspot.com/-vImShZH8tME/U6K4B6jFj5I/AAAAAAAABYg/bSTs6fdfmm4/s1600/gaia_clothing_bin_cult_donate_your_clothes_for_a_better_world.jpg',
 		'https://www.movingmountainstrust.org/media/201449/exploring-social-enterprise-3.jpg?height=0&width=0&quality=&mode=Crop&center=0.5,0.5&bgcolor=',
 		'https://npr.brightspotcdn.com/dims4/default/cd82aa1/2147483647/strip/true/crop/4608x3072+0+0/resize/880x587!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwrvo%2Ffiles%2F201310%2FBabaDogo3.jpg',
 		'https://st4.depositphotos.com/11089512/21554/i/450/depositphotos_215548860-stock-photo-donation-concept-woman-preparing-her.jpg',
-		'https://giveuswings.org/wp-content/uploads/2018/09/group-of-women-600x475.jpg',
+		'https://giveuswings.org/wp-content/uploads/2018/09/group-of-women-600x475.jpg'
 	];
 </script>
 
-
-
 <div class="events-page">
-	<h1 style:font-size="150%" style:font-weight="bold">Join Us for our upcoming events and make a difference in the community!</h1>
+	<h1 style:font-size="150%" style:font-weight="bold">
+		Join Us for our upcoming events and make a difference in the community!
+	</h1>
 
 	<div class="gallery">
 		{#each images as image, index}
@@ -29,7 +29,10 @@
 	</button>
 </div>
 
-
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
 
 <style>
 	.events-page {
@@ -47,7 +50,7 @@
 	.gallery img {
 		width: 100%;
 		height: auto;
-    object-fit: scale-down;
+		object-fit: scale-down;
 		object-position: center;
 	}
 
@@ -62,8 +65,3 @@
 		cursor: pointer;
 	}
 </style>
-
-<link
-	rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-/>
