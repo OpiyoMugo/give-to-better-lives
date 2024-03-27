6a400c417c76a1649c7ca5f1db7efde93bb3e1a1
Lazarus Mugo <mugolazarusk@gmail.com>
2024-03-27 15:02:31 +0300

    fix(sveltekit config): updates adapter
    git push

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,40 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			// fallback: 'index.html'
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	preprocess: vitePreprocess(),
+// 	kit: {
+// 		adapter: adapter({
+// 			// fallback: 'index.html'
 
-		}),
-		// prerender: {
-		// 	entries: [
-		// 	  '/home',
-		// 	  '/about',
-		// 	  '/events',
-		// 	  '/contacts',
-		// 	  '/donate',
-		// 	],
-		//   },
-	}
+// 		}),
+// 		// prerender: {
+// 		// 	entries: [
+// 		// 	  '/home',
+// 		// 	  '/about',
+// 		// 	  '/events',
+// 		// 	  '/contacts',
+// 		// 	  '/donate',
+// 		// 	],
+// 		//   },
+// 	}
+// };
+
+// export default config;
+
+
+
+import adapter from '@sveltejs/adapter-node';
+
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      out: 'build',
+      precompress: true,
+      envPrefix: ''
+    })
+  }
 };
 
-export default config;
