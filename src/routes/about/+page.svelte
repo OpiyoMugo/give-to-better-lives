7e89d464c1d90ef605b29f357970a5250267e16f
wes-ednilson <wesleydulo99@gmail.com>
2024-03-28 16:34:28 +0300

    fix: update about us page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,46 +1,47 @@
-<div class="image- md: flex">
+<div class="image- md: flex-">
 	<img src="hungry.png" alt="" />
 </div>
 
-<div class="text flex">
-	<h1>
-		<span style="color: orange; font-size: 24px; font-weight: bold;">Who</span>
-		<span style="color: green; font-size: 24px; font-weight: bold;">We Are</span>
-	</h1>
-	<p>
-		A Charity Organization founded to ensure that children and families most in need have one basic
-		commodity of life, “Clothing”.
-	</p>
-	<p>
-		We commit ourselves to helping families in the poor communities, the marginalized and
-		underprivileged to improve their lives and achieve lasting victory against poverty.
-	</p>
+<div class="py-10 px-3 bg-gradient-to-b flex items-center justify-center text-center">
+	<div class="text-center flex-col md:px-0">
+		<p class="uppercase text-3xl font-bold text-mainGold p-6">WHO WE ARE</p>
+		<p class=" justify-start items-start text-center flex">
+			Are A Charity Organization founded to ensure that children and families most in need have one
+			basic commodity of life, “Clothing”.
+		</p>
+	</div>
 </div>
 
-<div class="what-we-do">
-	<h2>
-		<span style="color: orange; font-size: 24px; font-weight: bold;">What</span>
-		<span style="color: green; font-size: 24px; font-weight: bold;">We Do</span>
-	</h2>
-	<div class="map-overlay">
-		<div class="box">
-			<h3 style="color: rgb(0, 200, 0);">Education</h3>
-			<p>
-				Our Volunteers ensure marginalized individuals are well-educated to survive their
-				environment.
-			</p>
-		</div>
-		<div class="box">
-			<h3 style="color: rgb(0, 200, 0);">Donate Clothes</h3>
-			<p>We fight poverty by ensuring needy kids and families afford proper clothing.</p>
-		</div>
-		<div class="box">
-			<h3 style="color: rgb(0, 200, 0);">Protect Children</h3>
-			<p>We fight poverty by ensuring needy kids and families afford proper clothing.</p>
-		</div>
-		<div class="box">
-			<h3 style="color: rgb(0, 200, 0);">Support Families</h3>
-			<p>We improve the living conditions of families so as to prevent possible calamities.</p>
-		</div>
+<div
+	class="flex flex-col items-center justify-center lg:flex-row md:justify-center md:items-center gap-4 my-8 lg:mx-28"
+>
+	<div class="bg-gray p-4 rounded-md py-4 m-3 lg:w-1/3 lg:h-[500px]">
+		<h3 class="text-xl font-bold mb-3">Education</h3>
+		<p class="mb-3">
+			Our Volunteers ensure marginalized individuals are well-educated to survive their environment.
+		</p>
+	</div>
+
+	<div class="bg-gray p-4 rounded-md py-4 m-3 lg:w-1/3 lg:h-[500px]">
+		<h3 class="text-xl font-bold mb-3">Donate Clothes</h3>
+		<p class="mb-3">We fight poverty by ensuring needy kids and families afford proper clothing.</p>
+	</div>
+	<div class="bg-gray p-4 rounded-md py-4 m-3 lg:w-1/3 lg:h-[500px]">
+		<h3 class="text-xl font-bold mb-3">Protect Children</h3>
+		<p class="mb-3">
+			We provide various services and support, such as education, healthcare, food, shelter, and
+			protection, to prevent children from becoming victims of exploitation or abuse. NGOs also
+			advocate for the rights and welfare of vulnerable children and work to ensure that they
+			receive the support and protection they need.
+		</p>
+	</div>
+
+	<div class="bg-white p-4 rounded-md py-4 m-3 lg:w-1/3 lg:h-[500px]">
+		<h3 class="text-xl font-bold mb-3">Support Families</h3>
+		<p class="mb-3">
+			Besides cloth donations, G.T.B.L has the mandate to facilitate and empower parents,
+			caregivers, and other stakeholders in Africa to provide a safe and nurturing environment for
+			children
+		</p>
 	</div>
 </div>
