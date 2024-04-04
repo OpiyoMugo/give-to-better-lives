de6723c2a7f9c766949fc962ab63d9135fb453b8
wes-ednilson <wesleydulo99@gmail.com>
2024-04-04 18:03:15 +0300

    update donate page payment methods

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-		import PayPal from '../../components/paypal.svelte';
+	import PayPal from '../../components/paypal.svelte';
 </script>
 
 <div class="container">
@@ -9,41 +9,57 @@
 			alt=""
 		/>
 	</div>
-	<div class="text-container">
-		<h2 style="font-size: 24px; font-weight: bold;">
-			Join Us in making a <span style="color: red; font-weight: bold;">DIFFERENCE</span>! Your
-			donation fuels hope, ignites change, and transforms lives
-		</h2>
 
+	<h2 class="text-xl items-align justify-center flex font-bold">
 		<p>
-			At Give To Better Lives, we believe in the power of generosity to create a brighter future for
-			all. Your donation is more than just a financial contribution; it's a lifeline for those in
-			need, a beacon of hope in times of darkness, and a testament to the compassion that unites us
-			all. With your support, we can continue to make a meaningful impact, reaching more
-			individuals, families, and communities in their times of greatest need. Together, we can build
-			a world where kindness knows no bounds and every person has the opportunity to thrive. Thank
-			you for being a vital part of our mission.
+			Join Us in making a DIFFERENCE Your donation fuels hope, ignites change, and transforms lives
 		</p>
-	</div>
+	</h2>
+
+	<p class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5">
+		At Give To Better Lives, we believe in the power of generosity to create a brighter future for
+		all. Your donation is more than just a financial contribution; it's a lifeline for those in
+		need, a beacon of hope in times of darkness, and a testament to the compassion that unites us
+		all. With your support, we can continue to make a meaningful impact, reaching more individuals,
+		families, and communities in their times of greatest need. Together, we can build a world where
+		kindness knows no bounds and every person has the opportunity to thrive. Thank you for being a
+		vital part of our mission.
+	</p>
 </div>
 
 <div class="flex items-center justify-center">
-
 	<PayPal />
-
 </div>
 
-<div class="container">
-	<div class="volunteer-section">
-			<h1 style:font-size="24px" style:font-weight="bold">Volunteer With Us</h1>
-			<div class="image-text-container">
-					<div class="overlay-container">
-							<img src="https://icma.org/sites/default/files/Volunteerism.jpeg" alt="" />
-							<div class="overlay"></div>
-					</div>
-					<p>Volunteering with Give To Better Lives is not just about giving back, it's also a rewarding and fulfilling experience for you. As a volunteer, you'll have the opportunity to connect with like-minded individuals, gain valuable skills, and make a tangible impact on the lives of others. Together, we can create positive change and build a stronger, more compassionate community.</p>
-			</div>
-	</div>
+<div class="py-8 px-4 pt-4 ring-offset-blue-400 flex items-center justify-center">
+	<h1 class="text-xl font-bold pt-5 m-2">
+		<p class="">OR</p>
+	</h1>
 </div>
 
+<div class=" flex items-center justify-center">
+	<h1 class="text-xl font-bold pt-4 m-2">
+		<p class="">DONATE VIA LIPA NA MPESA</p>
+	</h1>
+</div>
 
+<div class="flex items-center justify-center">
+	<div class="safaricom.png"></div>
+</div>
+
+<div class="flex items-center justify-center font-bold">
+	<h1 class="text-xl font-bold pt-4 m-2 bg">
+		<p class="">Donate Other Items</p>
+	</h1>
+</div>
+
+<div
+	
+></div>
+
+<p class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5">
+	Volunteering with Give To Better Lives is not just about giving back, it's also a rewarding and
+	fulfilling experience for you. As a volunteer, you'll have the opportunity to connect with
+	like-minded individuals, gain valuable skills, and make a tangible impact on the lives of others.
+	Together, we can create positive change and build a stronger, more compassionate community.
+</p>
