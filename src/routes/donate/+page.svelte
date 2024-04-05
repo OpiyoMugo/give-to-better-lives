d318bc759461dc798d867df0b077523cd71ee593
wes-ednilson <wesleydulo99@gmail.com>
2024-04-05 09:28:24 +0300

    updated donation payment system

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -2,30 +2,25 @@
 	import PayPal from '../../components/paypal.svelte';
 </script>
 
-<div class="container">
-	<div class="image-container">
-		<img
-			src="https://images.unsplash.com/photo-1593113598332-cd288d649433?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-			alt=""
-		/>
-	</div>
-
-	<h2 class="text-xl items-align justify-center flex font-bold">
-		<p>
-			Join Us in making a DIFFERENCE Your donation fuels hope, ignites change, and transforms lives
-		</p>
-	</h2>
-
-	<p class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5">
-		At Give To Better Lives, we believe in the power of generosity to create a brighter future for
-		all. Your donation is more than just a financial contribution; it's a lifeline for those in
-		need, a beacon of hope in times of darkness, and a testament to the compassion that unites us
-		all. With your support, we can continue to make a meaningful impact, reaching more individuals,
-		families, and communities in their times of greatest need. Together, we can build a world where
-		kindness knows no bounds and every person has the opportunity to thrive. Thank you for being a
-		vital part of our mission.
-	</p>
+<div class="flex h-50 pt-0 px-4 py-4">
+	<img src="truck.png" alt="" />
 </div>
+
+<h2 class="text-xl items-align justify-center flex font-bold pt-1 px-3">
+	<p>
+		Join Us in making a DIFFERENCE! Your donation fuels hope, ignites change, and transforms lives
+	</p>
+</h2>
+
+<p class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5 px-4">
+	At Give To Better Lives, we believe in the power of generosity to create a brighter future for
+	all. Your donation is more than just a financial contribution; it's a lifeline for those in need,
+	a beacon of hope in times of darkness, and a testament to the compassion that unites us all. With
+	your support, we can continue to make a meaningful impact, reaching more individuals, families,
+	and communities in their times of greatest need. Together, we can build a world where kindness
+	knows no bounds and every person has the opportunity to thrive. Thank you for being a vital part
+	of our mission.
+</p>
 
 <div class="flex items-center justify-center">
 	<PayPal />
@@ -37,29 +32,43 @@
 	</h1>
 </div>
 
-<div class=" flex items-center justify-center">
-	<h1 class="text-xl font-bold pt-4 m-2">
-		<p class="">DONATE VIA LIPA NA MPESA</p>
-	</h1>
+<div class="flex items-center justify-center">
+	<img src="/safaricom.png" alt="" class="h-20 pt-2 pb-0" />
 </div>
 
 <div class="flex items-center justify-center">
-	<div class="safaricom.png"></div>
+	<h2 class="flex-row pt-0 font-bold">
+		PAYBILL: <span style="color: green; font-weight: bold;">522533 </span>
+	</h2>
+</div>
+
+<div class="flex items-center justify-center">
+	<h2 class="flex-row pt-0 font-bold">
+		ACCOUNT NO: <span style="color: green; font-weight: bold;">7835338</span>
+	</h2>
+</div>
+
+<div class="flex items-center justify-center">
+	<h2 class="flex-row pt-0 font-bold">
+		BUSINESS NAME: <span style="color: black; font-weight: semi-bold;">BENARD MAKORI</span>
+	</h2>
 </div>
 
 <div class="flex items-center justify-center font-bold">
 	<h1 class="text-xl font-bold pt-4 m-2 bg">
-		<p class="">Donate Other Items</p>
+		<p class="">We also accept donations inform of Other Items</p>
 	</h1>
 </div>
 
-<div
-	
-></div>
+<div></div>
 
 <p class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5">
-	Volunteering with Give To Better Lives is not just about giving back, it's also a rewarding and
-	fulfilling experience for you. As a volunteer, you'll have the opportunity to connect with
-	like-minded individuals, gain valuable skills, and make a tangible impact on the lives of others.
-	Together, we can create positive change and build a stronger, more compassionate community.
+	At Give To Better Lives, we greatly appreciate all forms of support from our community. In
+	addition to monetary donations, we also welcome donations in the form of items that people are
+	willing to purchase. Whether it's gently used clothing, household items, toys, books, electronics
+	or any other goods, your in-kind donations can make a significant difference in the lives of those
+	we serve. These donations enable us to provide essential resources and support to individuals and
+	families in need. If you have items that you would like to donate, please reach out to us at
+	bmakori@givetobetterlives.me. Your generosity helps us continue our mission of Transforming lives
+	one garment at a time
 </p>
