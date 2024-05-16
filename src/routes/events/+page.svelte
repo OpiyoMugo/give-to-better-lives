c77c7b7c20d11ed9e5eb048a40b1a71358520fa3
wes-ednilson <wesleydulo99@gmail.com>
2024-05-16 22:02:19 +0300

    update events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,35 +1,16 @@
 <script>
 	let showMore = false;
-	const images = [
-		'launch.png',
-		'poverty.png',
-		'africa.png',
-		'box.png',
-		'fbcoverpage.png',
-		'dump.png'
-	];
+	const images = ['poverty.png', 'africa.png', 'box.png', 'fbcoverpage.png', 'dump.png'];
 </script>
 
 <div class="flex items-center justify-center">
 	<h1 class="text-2xl font-bold">
-		Join Us for our upcoming events and make a difference in the community! This is our journey.
+		Join Us for our upcoming events and make a difference in the community!
 	</h1>
 </div>
 
-<div class="flex items-start justify-center">
+<div class="flex items-start justify-start">
 	<h1 class="text-lg font-semibold">G.T.B.L Launch!</h1>
-</div>
-
-<div class="flex items-center">
-	<h2 class="text-xl flex items-align justify-center text-center md:text-lg mt-18 py-5 px-4 pt-0">
-		<p>
-			We are thrilled to share with you the exciting news: Give To Better Lives is officially live
-			and ready to make a difference! Since our inception, we have been tirelessly working towards
-			making a difference in the lives of children and families. Now, with great pride and
-			enthusiasm, we are delighted to announce that our doors are open, and our mission is set in
-			motion. G.T.B.L saw its first launch in May, 2024 in Kisii County, Kenya.
-		</p>
-	</h2>
 </div>
 <div class="flex flex-col items-align h-200px w-400px">
 	{#each images as image, index}
